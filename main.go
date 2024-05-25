452618df44b65c1e801ac13d1b06f59ca7edaba2
Bruno A. Muciño <mucinoab@gmail.com>
2024-05-25 16:17:11 -0600

    Add file compression to backend

--- main.go
+++ main.go
@@ -1,15 +1,18 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
 	"log"
 	"net/http"
+
+	"github.com/gin-contrib/gzip"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	port := ":8080"
 
 	router := gin.Default()
+	router.Use(gzip.Gzip(gzip.DefaultCompression))
 	router.StaticFS("/", http.Dir("./frontend/dist/"))
 
 	log.Print("Listening on http://localhost" + port)
