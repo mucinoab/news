8d1085e1986b963396511dc9b7b3ef6a585f3e8b
Bruno A. Muciño <mucinoab@gmail.com>
2024-05-27 19:38:46 -0600

    Insert emails into DB upon newsletter creation

--- newsletter/api.go
+++ newsletter/api.go
@@ -1,7 +1,14 @@
 package newsletter
 
 import (
+	"context"
+	"encoding/csv"
+	"errors"
+	"fmt"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -16,6 +23,64 @@ type Newsletter struct {
 
 	RecipientsFileName string `json:"recipients"`
 	Recipient          string `json:"recipient"`
+}
+
+func registerReceipients(nlID int64, nl Newsletter, db *Database) error {
+	var emails []string
+
+	if nl.Recipient != "" {
+		emails = append(emails, strings.TrimSpace(nl.Recipient))
+	}
+
+	if nl.RecipientsFileName != "" {
+		fileExtension := filepath.Ext(nl.RecipientsFileName)
+
+		if fileExtension != ".csv" && fileExtension != ".txt" {
+			return errors.New("invalid file format. Please provide a .csv or .txt file")
+		}
+
+		if fileExtension == ".csv" {
+			b, err := os.Open("/tmp/" + nl.RecipientsFileName)
+			if err != nil {
+				return errors.New("failed to open the file")
+			}
+			csvReader := csv.NewReader(b)
+			records, err := csvReader.ReadAll()
+			if err != nil {
+				return errors.New("unable to parse file as CSV. Make sure it is an email per row")
+			}
+
+			for _, record := range records {
+				emails = append(emails, record[0])
+			}
+
+		} else if fileExtension == ".txt" {
+			b, err := os.ReadFile("/tmp/" + nl.RecipientsFileName)
+
+			if err != nil {
+				return errors.New("failed to open the file")
+			}
+
+			emails = strings.Split(string(b), ",")
+		}
+	}
+
+	valueStrings := make([]string, 0, len(emails))
+	valueArgs := make([]interface{}, 0, len(emails)*2)
+
+	for _, email := range emails {
+		valueStrings = append(valueStrings, "(?, ?)")
+		valueArgs = append(valueArgs, strings.TrimSpace(email), nlID)
+	}
+
+	stmt := fmt.Sprintf("INSERT INTO RECEIPIENT (EMAIL, NEWSLETTER_ID) VALUES %s", strings.Join(valueStrings, ","))
+	_, err := db.driver.Exec(stmt, valueArgs...)
+
+	if err != nil {
+		return errors.New("failed to register recipients")
+	}
+
+	return nil
 }
 
 func Create(c *gin.Context) {
@@ -33,13 +98,42 @@ func Create(c *gin.Context) {
 	}
 	defer db.Close()
 
+	tx, err := db.driver.BeginTx(context.Background(), nil)
+	if err != nil {
+		c.String(http.StatusInternalServerError, "Failed to create the newsletter: %s", err)
+		return
+	}
+
 	query := `INSERT INTO NEWSLETTER
-              (NAME, DESCRIPTION, SUBJECT, CONTENT_ATTACHMENT_PATH )
+              (NAME, DESCRIPTION, SUBJECT, CONTENT_ATTACHMENT_PATH)
             VALUES (?, ?, ?, ?)`
-	_, err = db.driver.Exec(query, newsletter.Name, newsletter.Description, newsletter.Subject, newsletter.ContentFileName)
+	queryResult, err := db.driver.Exec(query, newsletter.Name, newsletter.Description, newsletter.Subject, newsletter.ContentFileName)
 
 	if err != nil {
 		c.String(http.StatusInternalServerError, "Failed to create the newsletter: %s", err)
+		tx.Rollback()
+		return
+	}
+
+	id, err := queryResult.LastInsertId()
+
+	if err != nil {
+		c.String(http.StatusInternalServerError, "Failed to get the newsletter ID: %s", err)
+		tx.Rollback()
+		return
+	}
+
+	err = registerReceipients(id, newsletter, db)
+
+	if err != nil {
+		c.String(http.StatusInternalServerError, err.Error())
+		tx.Rollback()
+		return
+	}
+
+	if err = tx.Commit(); err != nil {
+		c.String(http.StatusInternalServerError, "Failed to commit transaction", err)
+		tx.Rollback()
 		return
 	}
 
